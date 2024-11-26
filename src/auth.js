6260f96433915379bdb052ece97a5c8cfe3c416c
lomen-HaByeongHyun <hakim@lomen.kr>
2024-11-26 13:56:21 +0900

    apple env update

--- src/auth.js
+++ src/auth.js
@@ -10,29 +10,32 @@ async function getAppleToken() {
   const key = `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_SECRET}\n-----END PRIVATE KEY-----`;
 
   try {
-    console.log("success");
-    return new SignJWT({})
+    const token = new SignJWT({})
       .setAudience("https://appleid.apple.com")
-      .setIssuer(process.env.APPLE_TEAM_ID)
+      .setIssuer(process.env.AUTH_APPLE_TEAM_ID)
       .setIssuedAt(Math.floor(Date.now() / 1000))
       .setExpirationTime(Math.floor(Date.now() / 1000) + 3600 * 2)
-      .setSubject(process.env.APPLE_ID)
+      .setSubject(process.env.AUTH_APPLE_ID)
       .setProtectedHeader({
         alg: "ES256",
-        kid: process.env.APPLE_KEY_ID,
+        kid: process.env.AUTH_APPLE_KEY_ID,
       })
       .sign(createPrivateKey(key));
+
+    return token;
   } catch (error) {
     console.log("error", error);
   }
 }
+
+console.log("clientKey", await getAppleToken());
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Kakao,
     Naver,
     Apple({
-      clientId: process.env.APPLE_ID,
+      clientId: process.env.AUTH_APPLE_ID,
       clientSecret: async () => await getAppleToken(),
       profile(profile) {
         return {
