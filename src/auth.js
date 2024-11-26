33193ac124c774b5d5e8ca6a06c6bd1463e8576d
lomen-HaByeongHyun <hakim@lomen.kr>
2024-11-26 13:50:22 +0900

    debug

--- src/auth.js
+++ src/auth.js
@@ -1,31 +1,35 @@
 import NextAuth from "next-auth";
-import Kakao from "next-auth/providers/kakao";
-import Naver from "next-auth/providers/naver";
-import Apple from "next-auth/providers/apple";
+import AppleProvider from "next-auth/providers/apple";
 import { createPrivateKey } from "crypto";
-import process from "process";
 import { SignJWT } from "jose";
 
 async function getAppleToken() {
   const key = `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_SECRET}\n-----END PRIVATE KEY-----`;
-  return new SignJWT({})
-    .setAudience("https://appleid.apple.com")
-    .setIssuer(process.env.APPLE_TEAM_ID)
-    .setIssuedAt(Math.floor(Date.now() / 1000))
-    .setExpirationTime(Math.floor(Date.now() / 1000) + 3600 * 2)
-    .setSubject(process.env.APPLE_ID)
-    .setProtectedHeader({
-      alg: "ES256",
-      kid: process.env.APPLE_KEY_ID,
-    })
-    .sign(createPrivateKey(key));
+
+  try {
+    const token = await new SignJWT({})
+      .setAudience("https://appleid.apple.com")
+      .setIssuer(process.env.APPLE_TEAM_ID)
+      .setIssuedAt(Math.floor(Date.now() / 1000))
+      .setExpirationTime(Math.floor(Date.now() / 1000) + 3600 * 2)
+      .setSubject(process.env.APPLE_ID)
+      .setProtectedHeader({
+        alg: "ES256",
+        kid: process.env.APPLE_KEY_ID,
+      })
+      .sign(createPrivateKey(key));
+
+    console.log("Generated Apple Token:", token); // 디버깅용 로그
+    return token;
+  } catch (error) {
+    console.error("Error generating Apple Token:", error);
+    throw error;
+  }
 }
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export default NextAuth({
   providers: [
-    Kakao,
-    Naver,
-    Apple({
+    AppleProvider({
       clientId: process.env.APPLE_ID,
       clientSecret: async () => await getAppleToken(),
       profile(profile) {
@@ -37,21 +41,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
-  // session: {
-  //   strategy: "jwt",
-  //   maxAge: 30, // 30초
-  // },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // 로그인 성공 시 /login으로 이동
-      console.log(url, baseUrl);
-
-      if (url === "/auth/callback/kakao") {
-        return `${baseUrl}/login`;
-      }
-
-      // 로그인 실패 시 메인 페이지로 이동
-      return `${baseUrl}/`;
-    },
-  },
 });
