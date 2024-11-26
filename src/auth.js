b8770c99d8e0c2287f980e3083a036e6002b8c45
lomen-HaByeongHyun <hakim@lomen.kr>
2024-11-26 17:02:34 +0900

    apple update

--- src/auth.js
+++ src/auth.js
@@ -2,34 +2,39 @@ import NextAuth from "next-auth";
 import Kakao from "next-auth/providers/kakao";
 import Naver from "next-auth/providers/naver";
 import Apple from "next-auth/providers/apple";
-import { createPrivateKey } from "crypto";
+// import { createPrivateKey } from "crypto";
 import process from "process";
-import { SignJWT } from "jose";
+// import { SignJWT } from "jose";
 
-const getAppleToken = async () => {
-  const key = `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_SECRET}\n-----END PRIVATE KEY-----`;
+// const getAppleToken = async () => {
+//   const key = `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_SECRET}\n-----END PRIVATE KEY-----`;
 
-  try {
-    const token = new SignJWT({})
-      .setAudience("https://appleid.apple.com")
-      .setIssuer(process.env.AUTH_APPLE_TEAM_ID)
-      .setIssuedAt(Math.floor(Date.now() / 1000))
-      .setExpirationTime(Math.floor(Date.now() / 1000) + 3600 * 2)
-      .setSubject(process.env.AUTH_APPLE_ID)
-      .setProtectedHeader({
-        alg: "ES256",
-        kid: process.env.AUTH_APPLE_KEY_ID,
-      })
-      .sign(createPrivateKey(key));
+//   const token = new SignJWT({})
+//     .setAudience("https://appleid.apple.com")
+//     .setIssuer(process.env.AUTH_APPLE_TEAM_ID)
+//     .setIssuedAt(Date.now() / 1000)
+//     .setExpirationTime(Date.now() / 1000 + 3600 * 2)
+//     .setSubject(process.env.AUTH_APPLE_ID)
+//     .setProtectedHeader({
+//       alg: "ES256",
+//       kid: process.env.AUTH_APPLE_KEY_ID,
+//     })
+//     .sign(createPrivateKey(key));
 
-    return token;
-  } catch (error) {
-    console.log("error", error);
-  }
-};
+//   return token;
+// };
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   cookies: {
+    csrfToken: {
+      name: "next-auth.csrf-token",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
     pkceCodeVerifier: {
       name: "next-auth.pkce.code_verifier",
       options: {
@@ -39,32 +44,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         secure: true,
       },
     },
-    callbackUrl: {
-      name: `__Secure-next-auth.callback-url`,
-      options: {
-        httpOnly: false,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
   },
-
-  secret: process.env.AUTH_SECRET,
-
   providers: [
     Kakao,
     Naver,
     Apple({
       clientId: process.env.AUTH_APPLE_ID,
-      clientSecret: async () => await getAppleToken(),
-      // authorization: {
-      //   url: "https://appleid.apple.com/auth/authorize",
-      //   params: {
-      //     scope: "name email",
-      //     response_mode: "form_post",
-      //   },
-      // },
+      // clientSecret: async () => await getAppleToken(),
+      clientSecret: process.env.AUTH_APPLE_SECRET,
       profile(profile) {
         return {
           id: profile.sub,
@@ -83,17 +70,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   //   strategy: "jwt",
   //   maxAge: 30, // 30초
   // },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // 로그인 성공 시 /login으로 이동
-      console.log(url, baseUrl);
+  // callbacks: {
+  //   async redirect({ url, baseUrl }) {
+  //     // 로그인 성공 시 /login으로 이동
+  //     console.log(url, baseUrl);
 
-      if (url === "/auth/callback/kakao") {
-        return `${baseUrl}/login`;
-      }
+  //     if (url === "/auth/callback/kakao") {
+  //       return `${baseUrl}/login`;
+  //     }
 
-      // 로그인 실패 시 메인 페이지로 이동
-      return `${baseUrl}/`;
-    },
-  },
+  //     // 로그인 실패 시 메인 페이지로 이동
+  //     return `${baseUrl}/`;
+  //   },
+  // },
 });
